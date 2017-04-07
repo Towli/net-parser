3b0c4cd520ddc4f0059933445df86bfaabcf03c0
Alex Towli <alextowli@hotmail.co.uk>
2017-04-07 20:07:45 +0100

    convert indents to spaces (2 spaces)

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ var regex = "";
 var ParserFactory = new Parser.Factory();
 
 fs.readFile('./ping_test.txt', 'utf8', function(err, data) {
-	if (err) throw err;
-	var PingParser = ParserFactory.createParser('ping');
-	var output = PingParser.parse(data);
-	console.log(output);
+  if (err) throw err;
+  var PingParser = ParserFactory.createParser('ping');
+  var output = PingParser.parse(data);
+  console.log(output);
 });
 
 fs.readFile('./traceroute_test.txt', 'utf8', function(err, data) {
-	if (err) throw err;
-	var TracerouteParser = ParserFactory.createParser('traceroute');
-	var output = TracerouteParser.parse(data);
-	console.log(output);
+  if (err) throw err;
+  var TracerouteParser = ParserFactory.createParser('traceroute');
+  var output = TracerouteParser.parse(data);
+  console.log(output);
 });
--- src/parser.js
+++ src/parser.js
@@ -1,124 +1,123 @@
 
 class Parser {
-	constructor() {
-		console.log('Parser created!');
-	}
-	parse(data){}
+  constructor() {
+    console.log('Parser created!');
+  }
+  parse(data){}
 }
 
 class PingParser extends Parser {
-	constructor() {
-		super()
-		console.log('IAMA PingParser.');
-	}
-	getTimestamp(data) {
-		var regex = /[0-9]{10}/gi;
-		return data.match(regex);
-	}
-	getPacketLoss(data) {
-		var regex = /\d*.\d*% packet loss/gi;
-		return data.match(regex);
-	}
-	getIPAddress(data) {
-		var regex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/gi;
-		return data.match(regex)[0];
-	}
-	getRoundTripStats(data) {
-		var regex = /(round-trip|rtt.*)$/gi;
-		var RTStats = data.match(regex);
-	 	RTStats = RTStats.toString().match(/\d+.\d+/gi);
-		var min = RTStats[0];
-		var avg = RTStats[1];
-		var max = RTStats[2];
-		var stdDev = RTStats[3];
-		return {
-			min: min,
-			avg: avg,
-			max: max,
-			stdDev: stdDev
-		}
-	}
-	parse(data) {
-		return {
-			IPAddress: this.getIPAddress(data),
-			timestamp: this.getTimestamp(data),
-			packetLoss: this.getPacketLoss(data),
-			RTStats: this.getRoundTripStats(data)
-		}
-	}
+  constructor() {
+    super()
+    console.log('IAMA PingParser.');
+  }
+  getTimestamp(data) {
+    var regex = /[0-9]{10}/gi;
+    return data.match(regex);
+  }
+  getPacketLoss(data) {
+    var regex = /\d*.\d*% packet loss/gi;
+    return data.match(regex);
+  }
+  getIPAddress(data) {
+    var regex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/gi;
+    return data.match(regex)[0];
+  }
+  getRoundTripStats(data) {
+    var regex = /(round-trip|rtt.*)$/gi;
+    var RTStats = data.match(regex);
+    RTStats = RTStats.toString().match(/\d+.\d+/gi);
+    var min = RTStats[0];
+    var avg = RTStats[1];
+    var max = RTStats[2];
+    var stdDev = RTStats[3];
+    return {
+      min: min,
+      avg: avg,
+      max: max,
+      stdDev: stdDev
+    }
+  }
+  parse(data) {
+    return {
+      IPAddress: this.getIPAddress(data),
+      timestamp: this.getTimestamp(data),
+      packetLoss: this.getPacketLoss(data),
+      RTStats: this.getRoundTripStats(data)
+    }
+  }
 }
 
 class TracerouteParser extends Parser {
-	constructor() {
-		super()
-		console.log('IAMA TracerouteParser.');
+  constructor() {
+    super()
+    console.log('IAMA TracerouteParser.');
 
-		/* Private methods */
-		this.getHops = function(data) {
-			var regex = /([\n]\d\s*([^\n\r]*))|([\n] \s*([^\n\r]*))/gi;
-			return data.match(regex);
-		}
-		this.getRoundTripStats = function(data) {
-			var regex = /(\d+.\d+.\d+.\d+)|(\d+.\d+ ms)/gi;
-			var RTStats = data.match(regex);
-			var validStats = (RTStats);
-			if (validStats) {
-				return {
-					IP: RTStats[0],
-					RTT1: RTStats[2],
-					RTT2: RTStats[3],
-					RTT3: RTStats[4]
-				}
-			}
-			else
-				return;
-		}
-	}
-
-	getTimestamp(data) {
-		var regex = /[0-9]{10}/gi;
-		return data.match(regex);
-	}
-	getHopStats(data) {
-		var hops = this.getHops(data);
-		var hopStats = []
-		for (var i in hops) {
-			hopStats[i] = this.getRoundTripStats(hops[i]);
-		}
-		return hopStats;
-	}
-	getIPAddress(data) {
-		var regex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/gi;
-		return data.match(regex)[0];
-	}
-	parse(data) {
-		return {
-			IPAddress: this.getIPAddress(data),
-			timestamp: this.getTimestamp(data),
-			maxHops: "WGET's default is 30 (change through Parser API)",
-			hopStats: this.getHopStats(data)
-		}
-	}
+    /* Private methods */
+    this.getHops = function(data) {
+      var regex = /([\n]\d\s*([^\n\r]*))|([\n] \s*([^\n\r]*))/gi;
+      return data.match(regex);
+    }
+    this.getRoundTripStats = function(data) {
+      var regex = /(\d+.\d+.\d+.\d+)|(\d+.\d+ ms)/gi;
+      var RTStats = data.match(regex);
+      var validStats = (RTStats);
+      if (validStats) {
+        return {
+          IP: RTStats[0],
+          RTT1: RTStats[2],
+          RTT2: RTStats[3],
+          RTT3: RTStats[4]
+        }
+      }
+      else
+        return;
+    }
+  }
+  getTimestamp(data) {
+    var regex = /[0-9]{10}/gi;
+    return data.match(regex);
+  }
+  getHopStats(data) {
+    var hops = this.getHops(data);
+    var hopStats = []
+    for (var i in hops) {
+      hopStats[i] = this.getRoundTripStats(hops[i]);
+    }
+    return hopStats;
+  }
+  getIPAddress(data) {
+    var regex = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/gi;
+    return data.match(regex)[0];
+  }
+  parse(data) {
+    return {
+      IPAddress: this.getIPAddress(data),
+      timestamp: this.getTimestamp(data),
+      maxHops: "WGET's default is 30 (change through Parser API)",
+      hopStats: this.getHopStats(data)
+    }
+  }
 }
 
 class WGETParser extends Parser {
-	constructor() {
-		super()
-		console.log('IAMA WGETParser.');
-	}
+  constructor() {
+    super()
+    console.log('IAMA WGETParser.');
+  }
 }
 
 class Factory {
-	createParser(type) {
-		if (type.toUpperCase() === 'ping'.toUpperCase())
-			return new PingParser();
-		else
-		if (type.toUpperCase() === 'traceroute'.toUpperCase())
-			return new TracerouteParser();
-		else
-		if(type.toUpperCase() === 'wget'.toUpperCase())
-			return new WGETParser();
-	}
+  createParser(type) {
+    if (type.toUpperCase() === 'ping'.toUpperCase())
+      return new PingParser();
+    else
+    if (type.toUpperCase() === 'traceroute'.toUpperCase())
+      return new TracerouteParser();
+    else
+    if(type.toUpperCase() === 'wget'.toUpperCase())
+      return new WGETParser();
+  }
 }
 
 module.exports = { Parser, PingParser, TracerouteParser, WGETParser, Factory };
