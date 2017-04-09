fbe05381cac1d4cec846fbb429d7f6ed4ceb3d59
Alex Towli <alextowli@hotmail.co.uk>
2017-04-09 22:38:16 +0100

    take CLI arguments

--- src/index.js
+++ src/index.js
@@ -3,23 +3,43 @@ var fs = require('fs');
 var regex = "";
 var ParserFactory = new Parser.Factory();
 
-fs.readFile('./ping_test.txt', 'utf8', function(err, data) {
-  if (err) throw err;
-  var PingParser = ParserFactory.createParser('ping');
-  var output = PingParser.parse(data);
-  console.log(output);
-});
+var args = process.argv[2];
 
-fs.readFile('./traceroute_test.txt', 'utf8', function(err, data) {
-  if (err) throw err;
-  var TracerouteParser = ParserFactory.createParser('traceroute');
-  var output = TracerouteParser.parse(data);
-  console.log(output);
-});
+if (args == 'ping')
+  parsePing();
+else
+if (args == 'traceroute')
+  parseTraceroute();
+else
+if (args == 'wget')
+  parseWGET();
+else
+  console.log('No arguments given, terminating parser.');
 
-fs.readFile('./wget_test.txt', 'utf8', function(err, data) {
-  if (err) throw err;
-  var WGETParser = ParserFactory.createParser('wget');
-  var output = WGETParser.parse(data);
-  console.log(output);
-});
+
+function parsePing() {
+  fs.readFile('./ping_test.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    var PingParser = ParserFactory.createParser('ping');
+    var output = PingParser.parse(data);
+    console.log(output);
+  });
+}
+
+function parseTraceroute() {
+  fs.readFile('./traceroute_test.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    var TracerouteParser = ParserFactory.createParser('traceroute');
+    var output = TracerouteParser.parse(data);
+    console.log(output);
+  });
+}
+
+function parseWGET() {
+  fs.readFile('./wget_test.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    var WGETParser = ParserFactory.createParser('wget');
+    var output = WGETParser.parse(data);
+    console.log(output);
+  }); 
+}
